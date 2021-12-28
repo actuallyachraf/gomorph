66c3bd822acbeed00fa1d34f5e9314ed8fdcb998
actuallyachraf <actuallyachraf@gmail.com>
2021-12-28 23:59:30 +0100

    feat:rename branch to main

--- gaillier/gaillier_test.go
+++ gaillier/gaillier_test.go
@@ -1,17 +1,15 @@
-package gaillier_test
+package gaillier
 
 import (
 	"crypto/rand"
 	"math/big"
 	"testing"
-
-	"gomorph/gaillier"
 )
 
 func TestKeyGen(t *testing.T) {
-	puba, priva, err1 := gaillier.GenerateKeyPair(rand.Reader, 1024)
-	pubb, privb, err2 := gaillier.GenerateKeyPair(rand.Reader, 2048)
-	pubc, privc, err3 := gaillier.GenerateKeyPair(rand.Reader, 4096)
+	puba, priva, err1 := GenerateKeyPair(rand.Reader, 1024)
+	pubb, privb, err2 := GenerateKeyPair(rand.Reader, 2048)
+	pubc, privc, err3 := GenerateKeyPair(rand.Reader, 4096)
 
 	if err1 != nil || err2 != nil || err3 != nil {
 		t.Errorf("Error Generating Keypair :\n Size:1024  %v\nSize:2048  %v\nSize 4096  %v\n", err1, err2, err3)
@@ -32,20 +30,20 @@ func TestEncryptDecrypt(t *testing.T) {
 	case1 := new(big.Int).SetInt64(9132)
 	case2 := new(big.Int).SetInt64(1492)
 
-	pub, priv, err := gaillier.GenerateKeyPair(rand.Reader, 512)
+	pub, priv, err := GenerateKeyPair(rand.Reader, 512)
 
 	if err != nil {
 		t.Errorf("Error Generating Keypair")
 	}
-	encCase1, errCase1 := gaillier.Encrypt(pub, case1.Bytes())
-	encCase2, errCase2 := gaillier.Encrypt(pub, case2.Bytes())
+	encCase1, errCase1 := Encrypt(pub, case1.Bytes())
+	encCase2, errCase2 := Encrypt(pub, case2.Bytes())
 
 	if errCase1 != nil || errCase2 != nil {
 		t.Errorf("Error encrypting keypair %v \n %v", errCase1, errCase2)
 	}
 
-	d1, errDec1 := gaillier.Decrypt(priv, encCase1)
-	d2, errDec2 := gaillier.Decrypt(priv, encCase2)
+	d1, errDec1 := Decrypt(priv, encCase1)
+	d2, errDec2 := Decrypt(priv, encCase2)
 
 	decCase1 := new(big.Int).SetBytes(d1)
 	decCase2 := new(big.Int).SetBytes(d2)
@@ -59,24 +57,24 @@ func TestAdd(t *testing.T) {
 	case1 := new(big.Int).SetInt64(1)
 	case2 := new(big.Int).SetInt64(1)
 
-	pub, priv, err := gaillier.GenerateKeyPair(rand.Reader, 512)
+	pub, priv, err := GenerateKeyPair(rand.Reader, 512)
 
 	if err != nil {
 		t.Errorf("Error Generating Keypair")
 	}
 	//Encrypt
-	encCase1, err1 := gaillier.Encrypt(pub, case1.Bytes())
-	encCase2, err2 := gaillier.Encrypt(pub, case2.Bytes())
+	encCase1, err1 := Encrypt(pub, case1.Bytes())
+	encCase2, err2 := Encrypt(pub, case2.Bytes())
 
 	if err1 != nil || err2 != nil {
 		t.Errorf("Error Encrypting Integers")
 	}
 
-	res := gaillier.Add(pub, encCase1, encCase2)
+	res := Add(pub, encCase1, encCase2)
 
 	corr := new(big.Int).SetInt64(2)
 
-	decRes, err := gaillier.Decrypt(priv, res)
+	decRes, err := Decrypt(priv, res)
 	if err != nil {
 		t.Errorf("Failed to Decrypt Result got %v want %v with Error : %v", decRes, corr, err)
 	}
@@ -94,20 +92,20 @@ func TestAddConstant(t *testing.T) {
 	k := new(big.Int).SetInt64(10)
 	c := new(big.Int).SetInt64(32)
 
-	pub, priv, err := gaillier.GenerateKeyPair(rand.Reader, 102)
+	pub, priv, err := GenerateKeyPair(rand.Reader, 102)
 
 	if err != nil {
 		t.Errorf("Failed to generated keypair %v", err)
 	}
 
 	//encrypt
-	encC, err := gaillier.Encrypt(pub, c.Bytes())
+	encC, err := Encrypt(pub, c.Bytes())
 	if err != nil {
 		t.Errorf("Failed to encrypt c")
 	}
-	res := gaillier.AddConstant(pub, encC, k.Bytes())
+	res := AddConstant(pub, encC, k.Bytes())
 
-	decRes, err := gaillier.Decrypt(priv, res)
+	decRes, err := Decrypt(priv, res)
 	if err != nil {
 		t.Errorf("Failed to decrypt result")
 	}
@@ -125,20 +123,20 @@ func TestMul(t *testing.T) {
 	k := new(big.Int).SetInt64(10)
 	c := new(big.Int).SetInt64(32)
 
-	pub, priv, err := gaillier.GenerateKeyPair(rand.Reader, 102)
+	pub, priv, err := GenerateKeyPair(rand.Reader, 102)
 
 	if err != nil {
 		t.Errorf("Failed to generated keypair %v", err)
 	}
 
 	//encrypt
-	encC, err := gaillier.Encrypt(pub, c.Bytes())
+	encC, err := Encrypt(pub, c.Bytes())
 	if err != nil {
 		t.Errorf("Failed to encrypt c")
 	}
-	res := gaillier.Mul(pub, encC, k.Bytes())
+	res := Mul(pub, encC, k.Bytes())
 
-	decRes, err := gaillier.Decrypt(priv, res)
+	decRes, err := Decrypt(priv, res)
 	if err != nil {
 		t.Errorf("Failed to decrypt result")
 	}
@@ -149,14 +147,13 @@ func TestMul(t *testing.T) {
 		t.Errorf("Error Mul function want %d , got %d", corr, result)
 	}
 }
-
 func BenchmarkEncrypt(b *testing.B) {
-	pubKey, _, _ := gaillier.GenerateKeyPair(rand.Reader, 2048)
+	pubKey, _, _ := GenerateKeyPair(rand.Reader, 2048)
 	val := new(big.Int).SetInt64(int64(1234)).Bytes()
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, err := gaillier.Encrypt(pubKey, val)
+		_, err := Encrypt(pubKey, val)
 		if err != nil {
 			b.Fail()
 		}
